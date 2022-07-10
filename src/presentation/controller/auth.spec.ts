f7bc0a43fb4493b8a605f01517871aa458db486f
evanzs <evandrofidk@hotmail.com>
2022-07-10 15:05:02 -0300

    chore: add test with invalid email and invalid email class

--- src/presentation/controller/auth.spec.ts
+++ src/presentation/controller/auth.spec.ts
@@ -1,9 +1,29 @@
+import { EmailValidator } from "../protocols/email-validator";
 import { AuthController } from "./authController";
+import { InvalidParamsError } from "./errors/invalid-params-errors";
 import { MissingParamError } from "./errors/missing-param-error";
+
+interface SutTypes {
+  sut: AuthController;
+  emailValidadorStub: EmailValidator;
+}
+const makeSut = (): SutTypes => {
+  class EmailValidaorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true;
+    }
+  }
+  const emailValidadorStub = new EmailValidaorStub();
+  const sut = new AuthController(emailValidadorStub);
+  return {
+    sut,
+    emailValidadorStub,
+  };
+};
 
 describe("AuthController ", () => {
   test("Should return 400 if no name", () => {
-    const sut = new AuthController();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         email: "evandro@gmail.com",
@@ -18,7 +38,8 @@ describe("AuthController ", () => {
   });
 
   test("Should return 400 if no email", () => {
-    const sut = new AuthController();
+    const { sut } = makeSut();
+
     const httpRequest = {
       body: {
         name: "evandro",
@@ -33,7 +54,8 @@ describe("AuthController ", () => {
   });
 
   test("Should return 400 if no password", () => {
-    const sut = new AuthController();
+    const { sut } = makeSut();
+
     const httpRequest = {
       body: {
         name: "evandro",
@@ -48,7 +70,8 @@ describe("AuthController ", () => {
   });
 
   test("Should return 400 if no password confirmation", () => {
-    const sut = new AuthController();
+    const { sut } = makeSut();
+
     const httpRequest = {
       body: {
         name: "evandro",
@@ -60,7 +83,41 @@ describe("AuthController ", () => {
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(
-      new MissingParamError("password confirmation")
+      new MissingParamError("passwordConfirmation")
     );
+  });
+
+  test("Should return 400 if invalid email", () => {
+    const { sut, emailValidadorStub } = makeSut();
+    jest.spyOn(emailValidadorStub, "isValid").mockReturnValueOnce(false);
+    const httpRequest = {
+      body: {
+        name: "evandro",
+        email: "evandro@gmail.com",
+        password: "evandro123",
+        passwordConfirmation: "evandro123",
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new InvalidParamsError("email"));
+  });
+
+  test("Should call EmailValidador with correct email", () => {
+    const { sut, emailValidadorStub } = makeSut();
+
+    const isValidSpy = jest.spyOn(emailValidadorStub, "isValid");
+    const httpRequest = {
+      body: {
+        name: "evandro",
+        email: "evandro@gmail.com",
+        password: "evandro123",
+        passwordConfirmation: "evandro123",
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(isValidSpy).toHaveBeenCalledWith("evandro@gmail.com");
   });
 });
--- src/presentation/controller/authController.ts
+++ src/presentation/controller/authController.ts
@@ -1,15 +1,21 @@
 import { badRequest } from "../helpers/http-helper";
 import { Controller } from "../protocols/controller";
+import { EmailValidator } from "../protocols/email-validator";
 import { HttpRequest, HttpResponse } from "../protocols/http";
+import { InvalidParamsError } from "./errors/invalid-params-errors";
 import { MissingParamError } from "./errors/missing-param-error";
 
 export class AuthController implements Controller {
+  private readonly _emailValidator: EmailValidator;
+  constructor(emailValidator: EmailValidator) {
+    this._emailValidator = emailValidator;
+  }
   handle(httpRequest: HttpRequest): HttpResponse {
     const fieldsRequired: Array<string> = [
       "name",
       "email",
       "password",
-      "password confirmation",
+      "passwordConfirmation",
     ];
 
     for (const field of fieldsRequired) {
@@ -17,5 +23,9 @@ export class AuthController implements Controller {
         return badRequest(new MissingParamError(field));
       }
     }
+
+    const isValidEmail = this._emailValidator.isValid(httpRequest.body.email);
+
+    if (!isValidEmail) return badRequest(new InvalidParamsError("email"));
   }
 }
